650bae028b534ef0d05ec1efa4bac5ee63cdec43
ssselvig <seanssel@gmail.com>
2013-10-28 01:00:31 -0700

    organize directory

---
+++ examples/range-test.py
@@ -0,0 +1,5 @@
+num = 10
+
+for num in range(5): # so for assigns each element of the range so the iterative num object
+	print(num)
+print(num)
---
+++ examples/range-test3.py
@@ -0,0 +1,6 @@
+count = 0
+for letter in 'Snow!':
+    print('Letter # ' + str(count) + ' is ' + str(letter))
+    continue
+    count += 1
+print(count) 
