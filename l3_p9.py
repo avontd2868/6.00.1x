9cdfa11cdf2f2f602b85baaf27e3182104a312a8
ssselvig <seanssel@gmail.com>
2013-10-28 00:38:32 -0700

    finished l3_p9

--- l3_p9.py
+++ l3_p9.py
@@ -16,9 +16,45 @@ prompt user for hint:
     'l' - guess too low
     'c' - guess is correct
 
-if 'h':
-	low = guess
-elif 'l':
-	high = guess
-elif 'c':
-	game over, print guess
+loop:
+    if 'h':
+	    guess is the new high end
+    elif 'l':
+	    guess is the new low end
+    elif 'c':
+	    game over; break and print guess
+
+    bisect (guess = (high + low) / 2)
+    print guess
+
+'''
+
+low = 0
+high = 100
+guess = (high + low) / 2
+
+print "Please think of a number between 0 and 100!"
+
+while True:
+
+    print "\nIs your secret number " + str(guess) + "?"
+    print "Enter 'h' to indicate the guess is too high." 
+    print "Enter 'l' to indicate the guess is too low." 
+    print "Enter 'c' to indicate I guessed correctly."
+    hint = raw_input("\n> ")
+
+    if hint == 'h':
+    	high = guess
+    elif hint == 'l':
+    	low = guess
+    elif hint == 'c':
+    	print "Game over. Your secret number was: " + str(guess)
+    	break
+    else:
+    	"Say what?"
+
+    guess = (high + low) / 2
+
+
+
+
