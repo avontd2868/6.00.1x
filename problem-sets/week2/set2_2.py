954842ae6c727571bc93f4ca75d13e4801999e5c
ssselvig <seanssel@gmail.com>
2013-11-02 09:24:16 -0700

    finished Set 2, Problem 2

---
+++ problem-sets/week2/set2_2.py
@@ -0,0 +1,38 @@
+# Problem Set 2, #2: Paying Debt Off in a Year
+# Calculate the minimum fixed monthly payment needed in order to pay off 
+# credit card balance within 12 months.
+
+# Week2, Problem Set 2, #1: Paying the Minimum
+#
+# Calculate the credit card balance after one year if a person only pays the
+# minimum monthly payment required by the cc company each month.
+
+monthlyInterestRate = annualInterestRate / 12.0
+lowestPayment = 10
+
+
+def finalBalance(balance,lowestPayment):
+	for month in range(12):
+		minimumMonthlyPayment = balance * monthlyInterestRate
+		monthlyUnpaidBalance = balance - lowestPayment
+		balance = monthlyUnpaidBalance + (monthlyInterestRate * monthlyUnpaidBalance)
+		print balance
+	return balance
+
+
+while finalBalance(balance,lowestPayment) >= 0:
+	lowestPayment += 10
+    
+print "Lowest Payment: " + str(lowestPayment)
+
+
+    
+
+	
+
+
+	
+
+
+
+
