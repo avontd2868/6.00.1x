48d27328b025a6ba3eaa2b088c52853ab4fe2571
ssselvig <seanssel@gmail.com>
2013-11-03 12:28:29 -0800

    finished set 2, problem 3

--- problem-sets/week2/set2_3.py
+++ problem-sets/week2/set2_3.py
@@ -1,25 +1,36 @@
-# Week2, Problem Set 2, #3: Using bisection search to make the program faster
+# Week2, Problem Set 2, #3: find minimum payment using bisection search
 #
-# Eliminate slow code
+# Eliminate slow
 
-def finalBalance(balance):
-	low = balance / 12.0
-	high = (balance * (1 + (annualInterestRate/12.0))**12) / 12.0
-	lowestPayment = (high+low)/2.0
-
-    for month in range(12):
-        minimumMonthlyPayment = balance * (annualInterestRate/12.0)
-		monthlyUnpaidBalance = balance - lowestPayment
-		balance = monthlyUnpaidBalance + ((annualInterestRate/12.0) * monthlyUnpaidBalance)
-	while balance >= 0:
-		if lowestPayment * 12.0 < balance:
-			lowestPayment = low
-		else:
-			lowestPayment = high
-	return lowestPayment
-
-print finalBalance(3329)
+monthlyInterestRate = annualInterestRate/12.0
+low = balance / 12.0
+high = (balance * (1 + monthlyInterestRate)**12) / 12.0
+mid = (high+low) / 2.0
 
 
+def remainingBalance(monthlyPayment):
+	remainingBalance = balance
+	for month in range(12):
+		unpaidBalance = remainingBalance - monthlyPayment
+		remainingBalance = unpaidBalance + (monthlyInterestRate * unpaidBalance)
+	return remainingBalance
+
+for i in range(20):
+	if remainingBalance(mid) > 0.00:
+		low = mid
+	else:
+		high = mid
+	mid = (high + low) / 2.0
+
+print "Lowest Payment: " + str(round(mid,2))
+	
 
     
+
+
+	
+	
+
+
+
+
